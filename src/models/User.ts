77bceaf9e3bee44b649a22ac5bac68ec274c5654
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-05-21 23:18:57 +0100

    move to other laptop

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,24 @@
-import { equals } from 'typescript-is'
-import { PrismaClient, Primsa } from '@prisma/client';
+import { equals } from 'typescript-is';
+import { isEmail } from 'class-validator';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 export default function UserModel(prisma: PrismaClient['user']) {
-    return Object.assign(prisma, {});
+    const userArg = Prisma.validator<Prisma.UserArgs>()({
+        select: { name: true, email: true, password: true, role: true },
+    });
+    type User = Prisma.UserGetPayload<typeof userArg>;
+    return Object.assign(prisma, {
+        async validateType(data: User) {
+            if (!equals<User>(data)) {
+                throw new Error(`Expected a valid user input but got ${data}`);
+            } else if (!isEmail(data)) {
+                throw new Error(`${data.email} is an invalid email`);
+            }
+        },
+        async validateThenCreate(data: User) {
+            await this.validateType(data);
+            const user = await prisma.create({ data });
+            return { ...user, password: undefined };
+        },
+    });
 }
-
--- src/validators/User.ts
+++ src/validators/User.ts
@@ -1,23 +1,29 @@
-import { IsEmail, IsString, IsNotEmpty } from 'class-validator'
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+import { Prisma } from '@prisma/client';
 
-import { LoginFields } from '../interfaces'
+import { LoginFields } from '../interfaces';
 
-import IsValidFields from '.'
+import IsValidFields from '.';
+
+const userArg = Prisma.validator<Prisma.UserArgs>()({
+    select: { name: true, email: true, password: true, role: true },
+});
+type User = Prisma.UserGetPayload<typeof userArg>;
 
 export default class IsUser extends IsValidFields {
     @IsEmail(undefined, { context: { errorCode: 400 }, groups: ['login'] })
-    email!: string
+    email!: string;
 
     @IsString({ context: { errorCode: 400 }, groups: ['login'] })
     @IsNotEmpty({ context: { errorCode: 400 }, groups: ['login'] })
-    password?: string
+    password?: string;
 
     async isLogin({ email, password }: LoginFields) {
-        this.email = email
-        this.password = password
+        this.email = email;
+        this.password = password;
         return this.validateProps({
             validationError: { target: true },
             groups: ['login'],
-        })
+        });
     }
 }
